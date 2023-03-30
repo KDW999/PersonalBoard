67b798fb9f5eb86240446ad701e91b9bd1c66d39
KDW999 <hide4321@naver.com>
2023-03-30 13:15:02 +0900

    spring setting

---
+++ spring/board2/src/main/java/com/example/board2/board2/Board2Application.java
@@ -0,0 +1,15 @@
+package com.example.board2.board2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class Board2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board2Application.class, args);
+	}
+
+}
